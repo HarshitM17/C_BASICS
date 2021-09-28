b740be8b1781ce355281a4d55edf475c858b82c2
HarshitM17 <mathurharshit88@gmail.com>
2021-09-28 10:59:21 +0530

    done

---
+++ variables.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a = 25;
+    float b =76.5;
+    char c ='Y';
+    double d = 467.99;
+    
+    printf("Value of a is %d ,",a);
+    printf("Value of b is %f ,",b);
+    printf("Value of c is %c ,",c);
+    printf("Value of d is %lf",d);
+
+    return 0;
+    getch();
+}
