128dfb4836c7d675f768a69aceb68d9d930f1042
HarshitM17 <mathurharshit88@gmail.com>
2021-09-28 11:21:33 +0530

    done

---
+++ input_output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+
+    printf("The value of a is %d",a);
+    return 0;
+    getch();
+}
