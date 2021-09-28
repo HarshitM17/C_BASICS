e0c82954a8d0f751ea6c129e280e14c214611bfe
HarshitM17 <mathurharshit88@gmail.com>
2021-09-28 20:39:29 +0530

    done

---
+++ operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    int x = 30;
+    int y =20;
+
+    printf("x=%d ,",x);
+    printf("y=%d ,",y);
+    printf("x+y = %d ,",x+y);
+    printf("x-y = %d ,",x-y);
+    printf("x > y = %d ,",x>y);
+    printf("x < y = %d ,",x<y);
+    printf("x = y = %d",x==y);
+    return 0;
+    getch();
+}
---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter a value: ");
+    scanf("%d",&a);
+
+    printf("The +2 value of a is %d",a+2);
+    printf("The double value of a is %d",a+a);
+    return 0;
+    getch();
+
+}
