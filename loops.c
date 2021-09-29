dd3c7e65902693ab10e8f03f1bbf572a66b1a318
HarshitM17 <mathurharshit88@gmail.com>
2021-09-29 20:17:09 +0530

    h

---
+++ loops.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    // int x = 10;
+    // int i;
+
+    // for(i=1; i<=x;i++){
+    //     printf("%d\n",i);
+    // }
+    int x = 1;
+
+    while(x<=10){
+        printf("%d\n",x);
+
+        x++;
+    }
+}
