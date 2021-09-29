4261742cfb08710c85a2d948ed560bc53fa69c54
HarshitM17 <mathurharshit88@gmail.com>
2021-09-29 20:24:27 +0530

    h

---
+++ switch_case.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n =6;
+    switch(n){
+        case 1: printf("Case 1 it is");
+                break;
+        case 2: printf("Case 1 it is");
+                break;
+        case 3: printf("Case 1 it is");
+                break;
+        case 4: printf("Case 1 it is");
+                break;
+        case 5: printf("Case 1 it is");
+                break;
+        default: printf("No case is matching");
+    }
+}
