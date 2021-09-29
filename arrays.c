ba5805481ad32571908ac6b2392219653439a3f1
HarshitM17 <mathurharshit88@gmail.com>
2021-09-29 22:49:08 +0530

    hh

---
+++ arrays.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    // int a[5]={1,2,3,4,5};
+    // int i;
+
+    // for (i=0; i<5;i++)
+    // {
+    //     printf("%d\n",a[i]);
+    // }
+    char  a[7]={'H','a','r','s','h','i','t'};
+    int i;
+
+    for (i=0; i<7;i++)
+    {
+        printf("%c\n",a[i]);
+    }
+}
